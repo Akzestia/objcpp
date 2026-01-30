51ea1a0c8faa474e79c51e2a56e990731e307b48
アクゼスティア <akzestia@gmail.com>
2026-01-30 23:41:21 +0900

    Update objc.rs

--- src/objc.rs
+++ src/objc.rs
@@ -1,54 +1,70 @@
-use serde::Deserialize;
 use zed::LanguageServerId;
 use zed_extension_api::{
     self as zed, Command, DebugAdapterBinary, DebugRequest, DebugScenario, DebugTaskDefinition,
     Result, StartDebuggingRequestArgumentsRequest, TaskTemplate, Worktree, serde_json::Value,
 };
 
-#[derive(Debug, Deserialize)]
-struct ObjcLspExtension {
-    cached_binary_path: Option<String>,
-}
-
-impl ObjcLspExtension {
-    fn get_path_to_language_server_executable(
-        &mut self,
-        _language_server_id: &LanguageServerId,
-        worktree: &zed::Worktree,
-    ) -> Result<String> {
-        if let Some(path) = &self.cached_binary_path {
-            return Ok(path.clone());
-        }
-
-        let binary_name = "clangd";
-
-        if let Some(path) = worktree.which(&binary_name) {
-            self.cached_binary_path = Some(path.clone());
-            return Ok(path.clone());
-        }
-
-        Err(
-            "clangd binary wasn't found in $PATH. Make sure cqlls is installed on your system.\nCheck the install instructions on Github for further information: https://github.com/Akzestia/cqlls"
-                .to_owned(),
-        )
-    }
-}
+struct ObjcLspExtension;
 
 impl zed::Extension for ObjcLspExtension {
     fn new() -> Self {
-        Self {
-            cached_binary_path: None,
-        }
+        Self
     }
 
+    // language_server_command LICENSE
+    /*
+     * MIT License
+     *
+     * Copyright (c) 2025 blacktop
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in all
+     * copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     * SOFTWARE.
+     */
     fn language_server_command(
         &mut self,
-        language_server_id: &LanguageServerId,
+        _language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<Command> {
+        let path = worktree
+            .which("clangd")
+            .ok_or_else(|| "clangd must be installed and available in PATH".to_string())?;
+
         Ok(zed::Command {
-            command: self.get_path_to_language_server_executable(language_server_id, worktree)?,
-            args: vec![],
+            command: path,
+            args: vec![
+                // Use compile_commands.json if available
+                "--compile-commands-dir".to_string(),
+                worktree.root_path(),
+                // Enable background indexing for better performance
+                "--background-index".to_string(),
+                // Reduce log verbosity
+                "--log=error".to_string(),
+                // Enable header insertion with include-what-you-use style
+                "--header-insertion=iwyu".to_string(),
+                // Use #import instead of #include for Objective-C (important for ObjC!)
+                "--import-insertions".to_string(),
+                // Enable clang-tidy checks
+                "--clang-tidy".to_string(),
+                // Enable completion edits near cursor (dot-to-arrow conversion)
+                "--completion-style=detailed".to_string(),
+                // Set number of workers for background index
+                "-j=4".to_string(),
+            ],
             env: Default::default(),
         })
     }
